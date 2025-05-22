da08d95c06517d67869658cb1fb43ceca1863863
sarita <khatuasarita578@gmail.com>
2025-05-22 13:35:00 +0530

    service

--- frontend/src/pages/customer/Service.jsx
+++ frontend/src/pages/customer/Service.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import "./Service.css";
-const product = {
-  id: 1,
-  title: "Product Title",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
-  price: 9.99,
-  images: [
-    "https://placehold.co/500x500?font=roboto",
-    "https://placehold.co/400x400?font=roboto",
-    "https://placehold.co/300x300?font=roboto",
-    "https://placehold.co/200x200?font=roboto",
-  ],
-  stock: 10,
-};
+// const product = {
+//   id: 1,
+//   title: "Product Title",
+//   description:
+//     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
+//   price: 9.99,
+//   images: [
+//     "https://placehold.co/500x500?font=roboto",
+//     "https://placehold.co/400x400?font=roboto",
+//     "https://placehold.co/300x300?font=roboto",
+//     "https://placehold.co/200x200?font=roboto",
+//   ],
+//   stock: 10,
+// };
 
 const Service = () => {
   const [selectedImage, setSelectedImage] = useState(0);
