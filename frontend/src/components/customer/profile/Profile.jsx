5e87c858b9d253b59d1788a94c17c0afed7167b7
Vedant Vandra <vedantvandra@gmail git config --global user.email>
2025-06-11 18:50:32 +0530

    Profile main content

--- frontend/src/components/customer/profile/Profile.jsx
+++ frontend/src/components/customer/profile/Profile.jsx
@@ -31,7 +31,38 @@ function Profile() {
       <UserSideBar isOpen={isSidebarOpen} />
 
       <div className="main-content">
-        <h2>Main content</h2>
+        
+          <h2 className="booking-title">My Bookings</h2>
+          <div className="sort-btn">sort by <span>▼</span></div>
+      
+
+        <div className="booking-card">
+          <div className="booking-content">
+            <img
+              src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=600"
+              alt="DJ Service"
+              className="booking-image"
+            />
+
+            <div className="booking-details">
+              <div className="booking-left">
+                <h3>DJ Weeding Service</h3>
+                <p>Patia, Bhubaneswar, Odisha, India</p>
+                <p>Booking Date :- 10/06/2025</p>
+                <p>Event Date :- 10/06/2025</p>
+                <a href="#payment-details">Payment Details</a>
+              </div>
+
+              <div className="booking-right">
+                <div>Actual Amount :- <strong>₹50,000</strong></div>
+                <div className="strike">Paid Amount :-  <strong>₹3,000</strong></div>
+                <hr />
+                <div>Remaining Amount :- <strong>₹47,000</strong></div>
+                <p className="status">Pending<span className="dots">...</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
