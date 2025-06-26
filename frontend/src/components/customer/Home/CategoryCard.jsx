fbc6bd18c99135627fbd9c02199d4eb195664f22
sarita <khatuasarita578@gmail.com>
2025-06-26 16:55:55 +0530

    updated color and responsive of vendor dashboard

--- frontend/src/components/customer/Home/CategoryCard.jsx
+++ frontend/src/components/customer/Home/CategoryCard.jsx
@@ -1,43 +1,41 @@
 import React from "react";
 import "./CategoryCard.css";
+import { Link } from "react-router-dom";
 
 const CategoryCard = ({ category }) => {
+  
+
   return (
     <div className="courseCard">
       <span className="brandLabel">EventsBridge</span>
+      <Link to="/category">
+        <div className="imageWrapper">
+          <img
+            src={category.image}
+            alt={category.title}
+            className="courseImage"
+          />
 
-      <div className="imageWrapper">
-        <img
-          src={category.image}
-          alt={category.title}
-          className="courseImage"
-        />
+          {/* Dark Blur Overlay + Tagline */}
+          {category.tagline && (
+            <div className="taglineOverlay">
+              <span className="taglineText">{category.tagline}</span>
+            </div>
+          )}
+        </div>
 
-        {/* Dark Blur Overlay + Tagline */}
-        {category.tagline && (
-          <div className="taglineOverlay">
-            <span className="taglineText">{category.tagline}</span>
-          </div>
-        )}
-      </div>
+        <div className="courseContent">
+          <h3 className="courseTitle">
+            {/* <span className="courseIcon">{category.icon}</span> */}
+            <span className="courseText">{category.title}</span>
+          </h3>
 
-      <div className="courseContent">
-        <h3 className="courseTitle">
-          {/* <span className="courseIcon">{category.icon}</span> */}
-          <span className="courseText">{category.title}</span>
-        </h3>
-
-        {/* Tagline visible only in mobile/tablet */}
-        {category.tagline && (
-          <p className="courseTaglineResponsive">{category.tagline}</p>
-        )}
-
-        {/* {category.badge && (
-          <span className={`courseTag ${category.badge.toLowerCase()}`}>
-            {category.badge}
-          </span>
-        )} */}
-      </div>
+          {/* Tagline visible only in mobile/tablet */}
+          {category.tagline && (
+            <p className="courseTaglineResponsive">{category.tagline}</p>
+          )}
+        </div>
+      </Link>
     </div>
   );
 };
