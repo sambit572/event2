d07afb00f2fde426af09f1b692bfda75efbe6a3b
Rahul962160 <yadav962160@gmail.com>
2025-07-04 12:00:49 +0530

    updated ask me

--- frontend/src/pages/common/BackToTop.jsx
+++ frontend/src/pages/common/BackToTop.jsx
@@ -20,15 +20,14 @@ const BackToTop = () => {
   return show ? (
     <button
       onClick={scrollToTop}
-      className="hidden xl:flex fixed bottom-[150px] right-[32px] z-50 bg-[#f3c12d] hover:bg-[#ba79ff] text-white px-6 py-3 hover:text-black shadow-lg transition-all duration-300 hover:scale-105 items-center gap-2 text-sm font-semibold"
+      className="hidden xl:flex fixed bottom-[150px] right-[32px] z-10 bg-[#f3c12d] hover:bg-[#ba79ff] text-black px-6 py-3 hover:text-black shadow-lg transition-all duration-300 hover:scale-105 items-center gap-2 text-sm font-semibold"
       style={{
         borderRadius: "30px 30px 0px 30px",
         fontSize: "15px",
         fontWeight: "bold",
       }}
     >
-      <FaArrowUp size={15} />
-      Back to Top
+      <FaArrowUp size={30} />
     </button>
   ) : null;
 };
