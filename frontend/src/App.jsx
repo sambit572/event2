ad536fba7dbc6b463225a45e886a0b1b9561364f
Rahul962160 <yadav962160@gmail.com>
2025-07-23 12:36:18 +0530

    updated both sroll issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import Register from "./pages/common/Register.jsx";
 // Customer Pages
 import Home from "./pages/customer/Home";
 import ServiceList from "./pages/customer/ServiceList";
-import ReviewSlider from "./components/customer/Home/ReviewSlider.jsx";
+import ReviewSlider from "./components/customer/home/ReviewSlider.jsx";
 import ServiceDetails from "./pages/customer/ServiceDetails";
 
 import VendorLegalConsent from "./pages/vendor/VendorLegalConsent";
@@ -46,7 +46,7 @@ import ProtectedRoute from "./utils/ProtectedRoutes.jsx";
 import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
 
 import BackToTop from "./pages/common/BackToTop";
-import FaqSection from "./components/customer/Home/FaqSection.jsx";
+import FaqSection from "./components/customer/home/FaqSection.jsx";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import { setUser } from "./redux/UserSlice.js";
