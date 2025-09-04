2bc2186280cdf351f23d843c6f748e6c6c6dc887
Ubuntu <ubuntu@ip-172-31-29-82.ec2.internal>
2025-09-04 17:53:52 +0000

    from prod

--- frontend/src/components/vendor/DashBoardMain.jsx
+++ frontend/src/components/vendor/DashBoardMain.jsx
@@ -8,7 +8,8 @@ import socket from "../../socket/socketClient.js";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import PasswordInput from "./../../utils/PasswordInput";
+import PasswordInput from "./../../utils/PasswordInput.jsx";
+import { BACKEND_URL } from "../../utils/constant.js";
 function DashBoardMain() {
   const navigate = useNavigate();
 
@@ -39,7 +40,7 @@ function DashBoardMain() {
       const res = await axios.post(
         `${import.meta.env.VITE_BACKEND_URL}/vendors/verify-password`,
         {
-          assword,
+          password,
         },
         { withCredentials: true }
       );
